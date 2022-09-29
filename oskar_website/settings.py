37ec08b7ce8da297bd47893b732225e81d3f4367
Oskar <manaskarsanqu@gmail.com>
2022-09-29 15:28:17 +0200

    5

--- oskar_website/settings.py
+++ oskar_website/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os 
 import json
 
-with open ('/Users/kaitlin/Desktop/oskar project/website recap/Environments/oskar_website/config.json') as config_file:
+with open ('/Users/kaitlin/Desktop/oskar project/website recap/config.json') as config_file:
     config = json.load(config_file)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
